d2b072e43eac4d4ea557f64939a3fa40f4dd115d
Bhargav Virani <88129140+BMvirani@users.noreply.github.com>
2024-07-30 02:33:08 +0530

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCUaRF8aPo1X9BLiAYMeu2xYQwsODlWypA",
-    authDomain: "keep-clone-480b9.firebaseapp.com",
-    projectId: "keep-clone-480b9",
-    storageBucket: "keep-clone-480b9.appspot.com",
-    messagingSenderId: "167281972807",
-    appId: "1:167281972807:web:1117fa7b81de6d4b10c362",
-    measurementId: "G-CXE1BHGKW2"
+    apiKey: "AIP_KEY",
+    authDomain: "AUTHDOMAIN",
+    projectId: "PROJECTID",
+    storageBucket: "STORAGEBUCKET",
+    messagingSenderId: "MESSAGINGSENDERUD",
+    appId: "APPID",
+    measurementId: "MEASUREMENTID"
   };
 
 
